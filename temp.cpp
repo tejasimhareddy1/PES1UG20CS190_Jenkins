ae82b8035f1b330d4ef63fc3c56015b4689b4e21
tejasimhareddy1 <117919329+tejasimhareddy1@users.noreply.github.com>
2023-02-19 11:02:12 +0530

    Create temp.cpp

---
+++ temp.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, I am Srinivas Y of SEC-I\n";
+  cout << "I am in Task5 and my SRN is PES1UG20CS190\n";
+  return 0;
+}
